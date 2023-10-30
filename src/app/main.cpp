67b96617338faac4dd65e4563c107272ebd5d368
drfailer <drfailer.42@gmail.com>
2023-10-30 10:36:59 +0100

    update des stylesheets

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -9,12 +9,7 @@ int main(int argc, char *argv[])
   QApplication app(argc, argv);
   MainWindow   w;
   // on peut faire un steelsheet dans le code aussi :)
-  w.setStyleSheet("QPushButton                { background-color: #282828; color: white; font-size: 18px; border: 0px; }"
-                  "#QPushButton:hover         { background-color: #383838; }"
-                  "#QPushButton:checked       { background-color: #303030; }"
-                  "#QPushButton:checked:hover { background-color: #484848; }"
-                  "#QLabel                    { color: #F2F2F2; }"
-                  );
+  // w.setStyleSheet("#QLabel                    { color: #F2F2F2; }");
   w.show();
   return app.exec();
 }
--- src/app/widgets/gamelist.cpp
+++ src/app/widgets/gamelist.cpp
@@ -10,16 +10,15 @@ GameList::GameList(const QList<QString>& games, QWidget *parent):
 
     // adding the elements in the list of games
     if (games.empty()) {
-        // the text is in black ???
-        // TODO: paint it in white !
-        layout.addWidget(new QLabel("Na game available"), Qt::AlignLeft|Qt::AlignTop);
+        layout.addWidget(new QLabel("No game available"), Qt::AlignLeft|Qt::AlignTop);
     } else {
-        for (QString elt : games) {
+        // marche pas et je ne comprends pas pk
+        layout.setAlignment(Qt::AlignHCenter|Qt::AlignTop);
+        for (const QString& elt : games) {
             layout.addWidget(new QPushButton(elt));
             // TODO: connect button
             // TODO: change the stylesheet
         }
-        // layout.setAlignment(Qt::AlignHCenter|Qt::AlignTop);
     }
 }
 
