f4e33b4b216cd9faa766f81d5b07d650140570f8
Theo Cliquot <theo.cliquot@isima.fr>
2023-11-09 19:08:15 +0100

    prototype for mongoDB DAL

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -12,6 +12,12 @@ int main(int argc, char *argv[])
   // on peut faire un steelsheet dans le code aussi :)
   // w.setStyleSheet("#QLabel                    { color: #F2F2F2; }");
   w.show();
-  testt();
+
+  Dice d;
+  MongoDB::init("rollZZ");
+  //MongoDB::insertDocument(toDocument(d),"dice");
+  //MongoDB::requestAll("dice");
+  Dice d1 = toObject(MongoDB::requestOne("dice"));
+  std::cout << "faces: " << d1.getFaces() << " & diceNumber:" << d1.getDiceNumber() << std::endl;
   return app.exec();
 }
--- src/app/mongodb/mongodb.hpp
+++ src/app/mongodb/mongodb.hpp
@@ -1,6 +1,37 @@
 #ifndef MONGODB_HPP
 #define MONGODB_HPP
 
-void testt();
+#include <iostream>
+
+#include <bsoncxx/builder/basic/document.hpp>
+#include <bsoncxx/json.hpp>
+#include <mongocxx/client.hpp>
+#include <mongocxx/instance.hpp>
+#include <mongocxx/stdx.hpp>
+#include <mongocxx/uri.hpp>
+#include <mongocxx/exception/query_exception.hpp>
+#include<exception>
+#include"../CS/dice.h"
+#include<mongocxx/exception/bulk_write_exception.hpp>
+
+using bsoncxx::builder::basic::kvp;
+using bsoncxx::builder::basic::make_array;
+using bsoncxx::builder::basic::make_document;
+
+class MongoDB {
+
+public:
+static void init(std::string db_str);
+static void insertDocument(bsoncxx::document::view_or_value doc, std::string collection_str);
+static void requestAll(std::string collection_str);
+static bsoncxx::document::value requestOne(std::string collection_str);
+
+private:
+static mongocxx::database db;
+static mongocxx::client client;
+};
+
+bsoncxx::document::view_or_value toDocument(Dice d);
+Dice toObject(bsoncxx::document::value doc);
 
 #endif // MONGODB_HPP
