9145789a8570b7e211d7662819a54570351502ab
drfailer <drfailer.42@gmail.com>
2023-11-25 10:28:29 +0100

    fix indentation

--- src/app/widgets/cscreator/components/component.h
+++ src/app/widgets/cscreator/components/component.h
@@ -10,57 +10,58 @@
 
 namespace CSCreator {
 
-class Component : public QFrame {
+class Component : public QFrame
+{
   Q_OBJECT
 public:
-  explicit Component(const QString &title, QWidget *parent = nullptr);
-  virtual ~Component() {}
+    explicit Component(const QString &title, QWidget *parent = nullptr);
+    virtual ~Component() {}
 
-  /* methods ****************************************************************/
-  void bodyAdd(QWidget *wgt);
-  void bodyRemove(QWidget *wgt);
-  void bodyInsert(int index, QWidget *wgt);
-  int  bodyCount() const { return bodyLyt.count(); }
-  int  bodyIndexOf(QWidget *wgt) const { return bodyLyt.indexOf(wgt); }
+    /* methods ******************************************************************/
+    void bodyAdd(QWidget *wgt);
+    void bodyRemove(QWidget *wgt);
+    void bodyInsert(int index, QWidget *wgt);
+    int  bodyCount() const { return bodyLyt.count(); }
+    int  bodyIndexOf(QWidget *wgt) const { return bodyLyt.indexOf(wgt); }
 
-  /* accessors ***************************************************************/
-  QString getTitle() const { return title.text(); }
-  void    setTitle(const QString &newTitle) { title.setText(newTitle); }
+    /* accessors ****************************************************************/
+    QString getTitle() const { return title.text(); }
+    void    setTitle(const QString &newTitle) { title.setText(newTitle); }
 
-  /* setting function ********************************************************/
-  virtual void settingsPopup() = 0;
-  void         connectSettings();
+    /* setting function *********************************************************/
+    virtual void settingsPopup() = 0;
+    void         connectSettings();
 
 signals:
-  void moveUp();
-  void moveDown();
-  void remove();
+    void moveUp();
+    void moveDown();
+    void remove();
 
 private: // NOTE: everything should not be protected
-  QVBoxLayout mainLyt;
-  QHBoxLayout headerLyt; // contains the title and the buttons
-  QVBoxLayout bodyLyt;
-  QLabel      title;
-  QHBoxLayout titleLyt;
-  QHBoxLayout btnsLyt;
-  QPushButton removeBtn;
-  QPushButton moveUpBtn;
-  QPushButton moveDownBtn;
-  QPushButton settingsBtn;
+    QVBoxLayout mainLyt;
+    QHBoxLayout headerLyt; // contains the title and the buttons
+    QVBoxLayout bodyLyt;
+    QLabel      title;
+    QHBoxLayout titleLyt;
+    QHBoxLayout btnsLyt;
+    QPushButton removeBtn;
+    QPushButton moveUpBtn;
+    QPushButton moveDownBtn;
+    QPushButton settingsBtn;
 };
 
 #define genSettingsPopup(Class, popupVar, PopupClass, confirmCode, ...)        \
-  void Class::settingsPopup() {                                                \
-    if (popupVar == nullptr) {                                                 \
-      popupVar = new PopupClass(__VA_ARGS__);                                  \
-    }                                                                          \
-    popupVar->show();                                                          \
-    connect(popupVar, &PopupClass::confirm, this, [&](bool confirm) {          \
-      if (confirm) confirmCode;                                                \
-      delete popupVar;                                                         \
-      popupVar = nullptr;                                                      \
-    });                                                                        \
-  }
+    void Class::settingsPopup() {                                              \
+        if (popupVar == nullptr) {                                             \
+            popupVar = new PopupClass(__VA_ARGS__);                            \
+        }                                                                      \
+        popupVar->show();                                                      \
+        connect(popupVar, &PopupClass::confirm, this, [&](bool confirm) {      \
+            if (confirm) confirmCode;                                          \
+            delete popupVar;                                                   \
+            popupVar = nullptr;                                                \
+        });                                                                    \
+    }
 
 } // end namespace CSCreator
 
