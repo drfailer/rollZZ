2b908ce6e31b5ca645b7d54e9f183505f3786b15
drfailer <drfailer.42@gmail.com>
2023-12-30 11:06:00 +0100

    add edit placeholder for weapons

--- src/app/widgets/cseditor/components/weapon.cpp
+++ src/app/widgets/cseditor/components/weapon.cpp
@@ -4,8 +4,17 @@ namespace CSEditor {
 
 Weapon::Weapon(CS::Weapon* weapon):
     weapon(weapon),
-    layout(this)
+    layout(this),
+    nameEdit("name", this),
+    damageTypeEdit("damage type", this),
+    damageDiceEdit(true, this),
+    attackBonusEdit(this)
 {
+    layout.addWidget(&nameEdit);
+    layout.addWidget(&attackBonusEdit);
+    layout.addWidget(&damageDiceEdit);
+    layout.addWidget(&damageTypeEdit);
+    // TODO: compute default values for bonnuses (this should be done by the CS::Weapon)
     // TODO: connects
 }
 
--- src/app/widgets/cseditor/components/weapon.h
+++ src/app/widgets/cseditor/components/weapon.h
@@ -3,27 +3,28 @@
 
 #include "CS/weapon.h"
 #include "diceedit.h"
-#include <QWidget>
 #include <QBoxLayout>
+#include <QLineEdit>
 #include <QSpinBox>
+#include <QWidget>
 
 namespace CSEditor {
 
-class Weapon : public QWidget
-{
+class Weapon : public QWidget {
     Q_OBJECT
-public:
-    Weapon(CS::Weapon* weapon);
+  public:
+    Weapon(CS::Weapon *weapon);
     ~Weapon() = default;
 
-private:
-    CS::Weapon* weapon;
+  private:
+    CS::Weapon *weapon;
     QHBoxLayout layout;
-    DiceEdit attackDiceEdit;
+    QLineEdit nameEdit;
+    QLineEdit damageTypeEdit;
     DiceEdit damageDiceEdit;
     QSpinBox attackBonusEdit; // NOTE: this sould be computed automatically
 };
 
-} // end namespace CSCrator
+} // namespace CSEditor
 
 #endif
