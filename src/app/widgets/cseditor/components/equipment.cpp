8843e9ecfe4dba8189d16cc5e4b2d17dd423fb02
drfailer <drfailer.42@gmail.com>
2023-12-30 16:49:34 +0100

    update the equipment widget in the cseditor

--- src/app/widgets/cseditor/components/equipment.cpp
+++ src/app/widgets/cseditor/components/equipment.cpp
@@ -1,19 +1,52 @@
 #include "cseditor/components/equipment.h"
+#include "CS/item.h"
 #include "cseditor/components/component.h"
+#include "cseditor/components/item.h"
+#include <QPushButton>
 
 namespace CSEditor {
 
 /******************************************************************************/
-/*                                constructors                                */
+/*                          constructor & destructor                          */
 /******************************************************************************/
 
-Equipment::Equipment(CS::Equipment *equipment, QWidget *parent):
-    Component(equipment->getTitle(), parent),
-    equipment(equipment)
-{
+Equipment::Equipment(CS::Equipment *equipment, QWidget *parent)
+    : Component(equipment->getTitle(), parent), equipment(equipment),
+      addItemBtn("add item") {
     bodyAdd(&addItemBtn);
+    connect(&addItemBtn, &QPushButton::clicked, this, [&] { addItem(); });
 }
 
-// TODO: addItem
+Equipment::~Equipment() {
+    for (Item *item : items) {
+        delete item;
+    }
+    items.clear();
+}
+
+/******************************************************************************/
+/*                                manage items                                */
+/******************************************************************************/
+
+void Equipment::addItem() {
+    if (items.count() < equipment->getMaxItems()) {
+        CS::Item *item = new CS::Item();
+        Item *newItemWgt = new Item(item, equipment->getUseWeight(), this);
+
+        bodyInsert(bodyCount() - 1, newItemWgt);
+        items.push_back(newItemWgt);
+        connect(newItemWgt, &Item::remove, this, [&, item, wgt = newItemWgt] {
+            items.removeOne(wgt);
+            equipment->removeItem(item);
+            bodyRemove(wgt);
+            delete wgt;
+        });
+        // TODO not working yet
+        /* newItemWgt->connectWeightEdit(this, [&](int) { */
+        /*     equipment->setWeight(totalWeight()); */
+        /*     setTitle(equipment->getTitle() + weightStr()); */
+        /* }); */
+    }
+}
 
 } // end namespace CSEditor
--- src/app/widgets/cseditor/components/equipment.h
+++ src/app/widgets/cseditor/components/equipment.h
@@ -3,18 +3,40 @@
 
 #include "component.h"
 #include "CS/equipment.h"
+#include "cseditor/components/item.h"
 #include <QPushButton>
+#include <QList>
 
 namespace CSEditor {
 
+class Item;
+
 class Equipment : public Component
 {
+    Q_OBJECT
 public:
     Equipment(CS::Equipment *equipment, QWidget* parent = nullptr);
+    ~Equipment();
+    void addItem();
 
 private:
-    QPushButton addItemBtn;
     CS::Equipment *equipment = nullptr;
+    QPushButton addItemBtn;
+    QList<Item*> items;
+
+    /* helpter function *******************************************************/
+    int totalWeight() const {
+        int sum = 0;
+        for (Item *item : items) {
+            sum += item->getWeight();
+        }
+        return sum;
+    }
+
+    QString weightStr() const {
+        int weight = totalWeight();
+        return " (" + QString::number(weight) + "/" + QString::number(equipment->getMaxWeight()) + ")";
+    }
 };
 
 } // end namespace CSEditor
