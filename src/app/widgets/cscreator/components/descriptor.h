15b1a724ac86990e0580527ffe82034d108b1575
drfailer <drfailer.42@gmail.com>
2024-01-01 13:11:54 +0100

    add descriptor type in the cscreator widget

--- src/app/widgets/cscreator/components/descriptor.h
+++ src/app/widgets/cscreator/components/descriptor.h
@@ -1,27 +1,28 @@
 #ifndef CSCREATOR_DESCRIPTOR_H
 #define CSCREATOR_DESCRIPTOR_H
 
-#include "component.h"
 #include "CS/descriptor.h"
+#include "component.h"
 
 namespace CSCreator {
 
 class DescriptorPopup;
 
-class Descriptor : public Component
-{
+class Descriptor : public Component {
     Q_OBJECT
-public:
-    Descriptor(CS::Descriptor *descriptor, const QString& name = "name", QWidget* parent = nullptr);
+  public:
+    Descriptor(CS::Descriptor *descriptor, QWidget *parent = nullptr);
     void settingsPopup() override;
 
-private:
+  private:
     QLabel nameLabel;
+    QLabel typeLabel;
 
     DescriptorPopup *descriptorPopup = nullptr;
     CS::Descriptor *descriptor = nullptr;
 
-    void update(const QString& name);
+    void update(const QString &name, CS::DescriptorTypes type);
+    QString typeToString(CS::DescriptorTypes type) const;
 };
 
 } // end namespace CSCreator
