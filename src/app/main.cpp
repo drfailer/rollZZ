eab295ef4f69b5f59c9b5e3617a67be5b7db4422
drfailer <drfailer.42@gmail.com>
2024-01-15 11:25:40 +0100

    fix indenting of the main function

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -5,6 +5,8 @@
 #include <QFile>
 #include "mongodb/mongodb.hpp"
 
+/* #define MAIN_TEST */
+
 #ifndef MAIN_TEST
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
@@ -28,8 +30,26 @@ int main(int argc, char *argv[]) {
 }
 
 #else
+#include "component.h"
+#include "part.h"
 
-int main(int argc, char *argv[]) {
+int main(int , char **) {
+    CS::CS tree;
+
+    tree.deserializeFile("test.txt");
+    std::cout << tree.serialize() << std::endl;
+
+    for (CS::Part * part : tree.getParts()) {
+        std::cout << part->getName().toStdString() << std::endl;
+        for (CS::Section *section : part->getSections()) {
+            std::cout << "\t" << section->getTitle().toStdString() << std::endl;
+
+            for (CS::Component *component : section->getComponents()) {
+                std::cout << "\t\t" << component->getTitle().toStdString() << std::endl;
+            }
+        }
+    }
+
     return 0;
 }
 
