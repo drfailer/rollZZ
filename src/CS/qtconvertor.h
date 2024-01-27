715b213105fa900aa3f65972bfab7525936cd9e7
drfailer <drfailer.42@gmail.com>
2024-01-27 15:04:32 +0100

    update qtconvertor
    now serializer-cpp supports enum types so this methods are useless.

--- src/CS/qtconvertor.h
+++ src/CS/qtconvertor.h
@@ -2,23 +2,9 @@
 #define QTCONVERTOR_HPP
 #include "convertor.hpp"
 #include <QString>
-#include "descriptortypes.h"
 
 // TODO rename
 struct QtConvertor {
-    deserialize_custom_type(CS::DescriptorTypes, const std::string& str) {
-        std::istringstream iss(str);
-        int out;
-        iss >> out;
-        return (CS::DescriptorTypes) out;
-    }
-
-    serialize_custom_type(CS::DescriptorTypes type) {
-        std::ostringstream oss;
-        oss << type;
-        return oss.str();
-    }
-
     deserialize_custom_type(QString, const std::string& str) {
         return QString::fromUtf8(str.substr(1, str.size() - 2).c_str());
     }
